2f4626955ac440a99379b46f11a30c9c12c8757a
川鍋春輝 <125I039@stu.nit.ac.jp>
2026-05-15 09:21:58 +0900

    ggg

--- Assets/Script/Floor.cs
+++ Assets/Script/Floor.cs
@@ -8,13 +8,16 @@ using TMPro;
 public class Floor : MonoBehaviour
 {
     [SerializeField] private GameObject _floorPrefab;
+    [SerializeField] private GameObject _StartFloorPrefab;
+
+    private float lastGeneratedX = -100f;
 
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instantiate(_StartFloorPrefab, new Vector3(50, 0, -7), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -22,10 +25,12 @@ public class Floor : MonoBehaviour
     {
         if (Player._Xpos > 0)
         {
-            if (Player._Xpos % 100 == 0)
+            int currentX = (int)Player._Xpos;
+            if (currentX % 50 == 0 && currentX % 100 != 0 && currentX != lastGeneratedX)
             {
-                Instantiate(_floorPrefab, new Vector3(Player._Xpos + 100, 0, 0), Quaternion.identity);
+                Instantiate(_floorPrefab, new Vector3(Player._Xpos + 100, 0, -7), Quaternion.identity);
                 Debug.Log("床生成: " + (Player._Xpos + 100));
+                lastGeneratedX = currentX;
             }
         }
     }
